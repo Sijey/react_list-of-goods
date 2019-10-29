0009b644f648bca808a5d4a804a66590e0ce160b
Sijey <Popov040391@gmail.com>
2019-10-29 09:51:21 +0200

    Updates

---
+++ precache-manifest.43b1f8d4a15ab2e00b1c97637a989c0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00e27a941275284af48fc21f93aa5af",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "caa85a2e14d27223403d",
+    "url": "/react_list-of-goods/static/js/2.37d9ac16.chunk.js"
+  },
+  {
+    "revision": "ab0cebde20f0af5b0081",
+    "url": "/react_list-of-goods/static/js/main.9b90ccda.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
